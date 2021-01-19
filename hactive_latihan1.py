e2b7224dea9dd757d7f68bc25cbfa1cf40746f47
nitayolan <54371111+nitayolan@users.noreply.github.com>
2021-01-19 21:10:47 +0700

    Add files via upload

---
+++ hactive_latihan1.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print(356+1)
+
+
+# In[3]:
+
+
+print(10);
+print(type(10));
+
+
+# In[4]:
+
+
+print(4.2)
+print(type(4.2))
+
+print (4.)
+
+print (.2)
+
+
+# In[5]:
+
+
+print(0.4e7)
+
+
+# In[6]:
+
+
+print(4.2e-4)
+
+
+# In[11]:
+
+
+n=10012
+
+print(n)
+
+
+
+
+
+
+n
+
+
+# In[12]:
+
+
+n=10012
+
+n
+
+
+# In[15]:
+
+
+a= 2
+b= 4
+
+print (a**b)
+
+
+# In[23]:
+
+
+tas = ["aqua", 'baqua', 'caqua','daqua', 'eaqua']
+print (tas)
+
+
+# In[20]:
+
+
+print(tas[-3])
+
+
+# In[29]:
+
+
+print(tas[1:6])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
